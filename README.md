71156aa346bf7a6ad96a8f55b67be78a3c18e8e0
Wydglif <wydglif@gmail.com>
2019-10-11 16:05:12 -0500

    Addition of all files

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is my personal website. It was created for learning the deployment of Django apps.
---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/wydglifdorlus/Desktop/my_website/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
